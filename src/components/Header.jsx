7f66ac1f08a086175dc4a95b0b75330489914ebf
Chinmayee Sharma <chinmayeesharma911@gmail.com>
2024-03-11 00:34:26 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HighlightIcon from '@material-ui/icons/Highlight';
+
+function Header() {
+  return (
+    <header>
+      <h1><HighlightIcon />Keeper</h1>
+      <p>Keep all your notes at one place ✓</p>
+    </header>
+  );
+}
+
+export default Header;
